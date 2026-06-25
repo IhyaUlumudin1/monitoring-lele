8bb47cf3b920e4d6c5408f2144cb7f105a4a435e
IhyaUlumudin1 <ihyaulumudin728@gmail.com>
2026-06-25 17:36:39 +0700

    koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,8 +1,8 @@
 <?php
 $host     = "aws-1-ap-southeast-1.pooler.supabase.com"; 
-$port     = "6543"; // PENTING: Gunakan port 6543 untuk mode pooling
-$user     = "postgres.gxntzirycnuhykulhdpb"; // Gunakan user lengkap ini
-$password = "Monitoring_lele123"; // Ganti dengan password database kamu
+$port     = "6543"; // Gunakan port default pooler Supabase
+$user     = "postgres"; // Cukup tulis postgres saja, hapus teks titik dan ID project-nya
+$password = "Monitoring_lele123"; // Pastikan password baru yang sudah kamu ganti tadi
 $database = "postgres";
 
 $koneksi = pg_connect("host=$host port=$port dbname=$database user=$user password=$password");
